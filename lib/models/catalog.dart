3c983b27d0199ef2de11e6703067cb6286cf4cfd
tech-nirav <21it1@svitvasad.ac.in>
2023-03-11 23:34:06 +0530

    Fetch the json data

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,29 +1,46 @@
-class CatalogModel {
-  static final Items = [
-    Item(
-        id: 1,
-        name: "iPhone 12 Pro",
-        desc: "Apple iPhone 12th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-  ];
-}
+// class CatalogModel {
+//   static final items = [];
+// }
 
-class Item {
-  final int id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
+// class Item {
+//   final int id;
+//   final String name;
+//   final String desc;
+//   final num price;
+//   final String color;
+//   final String image;
 
-  Item(
+//   Item(
+//       {required this.id,
+//       required this.name,
+//       required this.desc,
+//       required this.price,
+//       required this.color,
+//       required this.image});
+// }
+
+class catalogModel {
+  int? id;
+  String? name;
+  String? desc;
+  num? price;
+  String? color;
+  String? image;
+
+  catalogModel(
       {required this.id,
       required this.name,
       required this.desc,
       required this.price,
       required this.color,
       required this.image});
+
+  catalogModel.fromJson(Map<String, dynamic> json) {
+    id = json["id"];
+    name = json["name"];
+    desc = json["desc"];
+    price = json["price"];
+    color = json["color"];
+    image = json["image"];
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,14 @@
+// import "dart:developer";
+// import "../widgets/ItemWidget.dart";
+
+import "dart:convert";
 import "package:flutter/material.dart";
 import "package:myfirstproject/models/catalog.dart";
-import "../widgets/ItemWidget.dart";
 import "../widgets/drawer.dart";
+import "package:flutter/services.dart" as rootBundle;
 
 class HomePage extends StatelessWidget {
-  final dummyList = List.generate(50, (index) => CatalogModel.Items[0]);
+  // final dummyList = List.generate(50, (index) => CatalogModel.Items[0]);
 
   // const HomePage({super.key});
 
@@ -18,17 +22,56 @@ class HomePage extends StatelessWidget {
       // body: Center(
       //   child: Text("Welcome to Bugs first $idk App"),
       // ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dummyList.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                item: dummyList[index],
+      // body: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: ListView.builder(
+      //       itemCount: CatalogModel.items.length,
+      //       itemBuilder: (context, index) {
+      //         return ItemWidget(
+      //           item: CatalogModel.items[index],
+      //         );
+      //       }),
+      // ),
+      body: FutureBuilder(
+          future: ReadJsonData(),
+          builder: (index, data) {
+            if (data.hasError) {
+              return Text("${data.error}");
+            }
+            if (data.hasData) {
+              var items = data.data as List<catalogModel>;
+              return ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        leading: Image.network(items[index].image.toString()),
+                        title: Text(items[index].name.toString()),
+                        subtitle: Text(items[index].desc.toString()),
+                        trailing: Text(
+                          "\$${items[index].price.toString()}",
+                          style: const TextStyle(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
               );
-            }),
-      ),
+            }
+          }),
       drawer: const MyDrawer(),
     );
   }
+}
+
+Future<List<catalogModel>> ReadJsonData() async {
+  final jsondata =
+      await rootBundle.rootBundle.loadString("assets/files/catalog.json");
+  final list = json.decode(jsondata) as List<dynamic>;
+  return list.map((e) => catalogModel.fromJson(e)).toList();
 }
